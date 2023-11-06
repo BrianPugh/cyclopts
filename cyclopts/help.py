c2b5b8bcb11acc351887a9b590130003b6886434
Brian Pugh <bnp117@gmail.com>
2023-11-05 18:43:11 -0800

    misc

--- cyclopts/help.py
+++ cyclopts/help.py
@@ -1,4 +1,6 @@
+import argparse
 from typing import Callable, Iterable, List, Optional, Tuple
+from itertools import chain
 
 from rich import box
 from rich.console import Console
@@ -10,20 +12,26 @@ from cyclopts.docstring import DocString
 from cyclopts.exceptions import CycloptsError, UnreachableError
 
 
-def _create_two_col_panel(title: str, col_1_strings: Iterable[str]):
-    # Calculate maximum width
-    width = max(len(s) for s in col_1_strings)
+class SilentRich:
+    """Dummy object that causes nothing to be printed."""
 
+    def __rich_console__(self, console, options):
+        # This generator yields nothing, so 'rich' will print nothing for this object.
+        if False:
+            yield
+
+
+_silent = SilentRich()
+
+
+def _create_panel_table(**kwargs):
     table = Table.grid(padding=(0, 1))
-    table.add_column(justify="left", no_wrap=True, width=width)
-    table.add_column(justify="left", no_wrap=False)
-
     panel = Panel(
         table,
         box=box.ROUNDED,
         expand=True,
-        title=title,
         title_align="left",
+        **kwargs,
     )
     return panel, table
 
@@ -63,11 +71,68 @@ def _format_arguments(self, function):
     raise NotImplementedError
 
 
+def _format_global_arguments(self, function):
+    panel, table = _create_panel_table()
+    for action in self.argparse._actions:
+        # Most _StoreAction attributes default are None.
+        breakpoint()
+
+
+def _format_global_options(self, function):
+    actions = [x for x in self.argparse._actions if x.option_strings]
+    if not actions:
+        return _silent
+
+    has_required = any(x.required for x in actions)
+    options, options_short = [], []
+    for action in actions:
+        _options, _options_short = [], []
+        for s in action.option_strings:
+            if s.startswith("--"):
+                _options.append(s)
+            else:
+                _options_short.append(s)
+        options.append(",".join(_options))
+        options_short.append(",".join(_options_short))
+
+    has_short = any(options_short)
+
+    panel, table = _create_panel_table(title="Global Options")
+    if has_required:
+        table.add_column(justify="left", width=1, style="red bold")
+    table.add_column(justify="left", width=max(len(x) for x in options), style="cyan")
+    if has_short:
+        table.add_column(justify="left", width=max(len(x) for x in options_short) + 1, style="green")
+    table.add_column(justify="left")
+
+    # Populate the table
+    for action, option, option_short in zip(actions, options, options_short):
+        row_args = []
+        if has_required:
+            row_args.append("*" if action.required else "")
+        row_args.append(option)
+        if has_short:
+            row_args.append(option_short)
+        row_args.append(action.help)  # TODO: probably want to add default & choices here.
+
+        table.add_row(*row_args)
+
+    return panel
+
+
 def _format_options(self, function):
-    for command_name, command_fn in self._commands.items():
-        pass
-    breakpoint()
-    panel, table = _create_two_col_panel("Options")
+    # --option -o limitations helpstring
+    if function is None:
+        for command_name, command_fn in self._commands.items():
+            pass
+        breakpoint()
+
+        raise NotImplementedError
+    else:
+        # Check the global argparse
+        breakpoint()
+        panel, table = _create_two_col_panel("Options")
+        raise NotImplementedError
 
     raise NotImplementedError
 
@@ -86,6 +151,10 @@ class HelpMixin:
 
         console.print(_format_usage(self, function))
         console.print(_format_doc(self, function))
+        # console.print(_format_global_arguments(self, function))
+        console.print(_format_global_options(self, function))
+        breakpoint()
+        console.print(_format_options(self, function))
         breakpoint()
         # console.print(_format_options(self))
         return
