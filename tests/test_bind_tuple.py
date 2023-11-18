803dd626872becf59ebeb52e70a18962500c38c5
Brian Pugh <bnp117@gmail.com>
2023-11-17 17:10:20 -0800

    misc

--- tests/test_bind_tuple.py
+++ tests/test_bind_tuple.py
@@ -3,6 +3,8 @@ from typing import Tuple
 
 import pytest
 
+from cyclopts.exceptions import MissingArgumentError
+
 
 @pytest.mark.parametrize(
     "cmd_str",
@@ -12,7 +14,7 @@ import pytest
         "--color 80 160 255 --coordinates 1 2",
     ],
 )
-def test_(app, cmd_str):
+def test_bind_tuple_basic(app, cmd_str):
     @app.default
     def foo(coordinates: Tuple[int, int], color: Tuple[int, int, int]):
         pass
@@ -23,3 +25,20 @@ def test_(app, cmd_str):
     actual_command, actual_bind = app.parse_args(cmd_str)
     assert actual_command == foo
     assert actual_bind == expected_bind
+
+
+@pytest.mark.skip(reason="Need to implement solution.")
+@pytest.mark.parametrize(
+    "cmd_str",
+    [
+        "1",
+        "--coordinates 1",
+    ],
+)
+def test_bind_tuple_insufficient_tokens(app, cmd_str):
+    @app.default
+    def foo(coordinates: Tuple[int, int]):
+        pass
+
+    with pytest.raises(MissingArgumentError):
+        app.parse_args(cmd_str)
