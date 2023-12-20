c8dbbb6a0c802d326b55ced7f5c75d827e950d13
Brian Pugh <bnp117@gmail.com>
2023-12-20 13:23:09 -0800

    initial ParameterDict implementation

--- cyclopts/utils.py
+++ cyclopts/utils.py
@@ -1,4 +1,7 @@
 import functools
+import inspect
+from collections.abc import MutableMapping
+from typing import Any, Iterator
 
 
 def record_init_kwargs(target: str):
@@ -17,3 +20,36 @@ def record_init_kwargs(target: str):
         return cls
 
     return decorator
+
+
+class ParameterDict(MutableMapping):
+    """A dictionary implementation that can handle ``inspect.Parameter`` as keys.
+
+    Traditional dictionaries don't always work because ``inspect.Parameter.default`` may be mutable.
+    """
+
+    def __init__(self):
+        self.store = {}
+
+    def _param_key(self, param: inspect.Parameter) -> tuple:
+        if not isinstance(param, inspect.Parameter):
+            raise TypeError("Key must be an inspect.Parameter")
+        return (param.name, param.kind, param.annotation)
+
+    def __getitem__(self, key: inspect.Parameter) -> Any:
+        return self.store[self._param_key(key)]
+
+    def __setitem__(self, key: inspect.Parameter, value: Any) -> None:
+        self.store[self._param_key(key)] = value
+
+    def __delitem__(self, key: inspect.Parameter) -> None:
+        del self.store[self._param_key(key)]
+
+    def __iter__(self) -> Iterator[inspect.Parameter]:
+        return iter(self.store)
+
+    def __len__(self) -> int:
+        return len(self.store)
+
+    def setdefault(self, key: inspect.Parameter, default: Any = None) -> Any:
+        return self.store.setdefault(self._param_key(key), default)
