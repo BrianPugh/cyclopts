f73f3bec6e6e1ee7de399b0c75ccae0b8d0837a8
Brian Pugh <bnp117@gmail.com>
2023-12-27 13:53:43 -0500

    move Group documentation to rst.

--- cyclopts/group.py
+++ cyclopts/group.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import TYPE_CHECKING, Callable, List, Literal, Optional, Union
+from typing import TYPE_CHECKING, Iterable, List, Literal, Optional, Union
 
 from attrs import define, field
 
@@ -7,50 +7,31 @@ if TYPE_CHECKING:
     from cyclopts.core import App
     from cyclopts.parameter import Parameter
 
+from cyclopts.coercion import to_tuple_converter
+from cyclopts.protocols import Validator
+
 
 @define
 class Group:
     name: str
-    """
-    Group name used for the help-panel and for group-referenced-by-string.
-    """
 
     help: str = ""
-    """
-    Additional documentation show on help screen.
-    """
 
     # All below parameters are keyword-only
+    show: bool = field(default=True, kw_only=True)
 
-    validator: Optional[Callable] = field(default=None, kw_only=True)
-    """
-    A callable where the CLI-provided group variables will be keyword-unpacked, regardless
-    of their positional/keyword type in the command function signature.
-
-    .. code-block:: python
-
-        def validator(**kwargs):
-            "Raise an exception if something is invalid."
-
-    *Not invoked on command groups.*
-    """
+    validator: Union[None, Validator, Iterable[Validator]] = field(default=None, converter=to_tuple_converter)
 
     default_parameter: Optional["Parameter"] = field(default=None, kw_only=True)
-    """
-    Default Parameter in the parameter-resolution-stack that goes
-    between ``app.default_parameter`` and the function signature's Annotated Parameter.
-    """
 
     default: Optional[Literal["Arguments", "Parameters", "Commands"]] = field(default=None, kw_only=True)
-    """
-    Only one group registered to an app can have each non-``None`` option.
-    """
 
     # Private Internal Use
     _children: List[Union[inspect.Parameter, "App"]] = field(factory=list, init=False)
     """
     List of ``inspect.Parameter`` or ``App`` (for commands) included in the group.
     Externally populated.
+    This is for private internal use and is initialized as an empty list.
     """
 
     def __str__(self):
