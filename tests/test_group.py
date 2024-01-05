99a22c38321c97376ed050d2ac72b1a42136bf85
Brian Pugh <bnp117@gmail.com>
2024-01-04 16:56:53 -0800

    invoke group validators

--- tests/test_group.py
+++ tests/test_group.py
@@ -1,6 +1,7 @@
 import inspect
 import sys
 from typing import Union
+from unittest.mock import Mock
 
 import pytest
 
@@ -42,8 +43,7 @@ def test_group_default_parameter_converter(app):
 
 
 def test_group_default_parameter_validator(app):
-    def validator(type_, value):
-        raise SentinelError
+    validator = Mock()
 
     food_group = Group("Food", default_parameter=Parameter(validator=validator))
 
@@ -51,5 +51,23 @@ def test_group_default_parameter_validator(app):
     def foo(ice_cream: Annotated[str, Parameter(group=food_group)]):
         pass
 
-    with pytest.raises(SentinelError):
-        app.parse_args("chocolate")
+    app.parse_args("chocolate")
+    validator.assert_called_once()
+
+
+def test_group_validator(app):
+    validator = Mock()
+
+    group = Group("Spices", validator=validator)
+
+    @app.default
+    def foo(
+        salt: Annotated[bool, Parameter("--rock-salt", group=group)] = False,
+        pepper: Annotated[bool, Parameter("--peppercorn", group=group)] = False,
+        ketchup: bool = False,
+    ):
+        pass
+
+    app.parse_args("--rock-salt --peppercorn --ketchup")
+
+    validator.assert_called_once_with(salt=True, pepper=True)
