572d8375e9659854e4399037c135b64efa398655
Brian Pugh <bnp117@gmail.com>
2024-01-07 12:27:13 -0800

    reduce tests

--- tests/test_bind_env_var.py
+++ tests/test_bind_env_var.py
@@ -12,57 +12,38 @@ else:
 from cyclopts import MissingArgumentError, Parameter
 
 
-def test_env_var_unset_use_signature_default(app):
+def test_env_var_unset_use_signature_default(app, assert_parse_args):
     @app.default
     def foo(bar: Annotated[int, Parameter(env_var="BAR")] = 123):
         pass
 
     os.environ.pop("BAR", None)
-
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind()
-
-    actual_command, actual_bind = app.parse_args([])
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "")
 
 
-def test_env_var_set_use_env_var(app):
+def test_env_var_set_use_env_var(app, assert_parse_args):
     @app.default
     def foo(bar: Annotated[int, Parameter(env_var="BAR")] = 123):
         pass
 
     os.environ["BAR"] = "456"
-
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind(456)
-
-    actual_command, actual_bind = app.parse_args([])
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "", 456)
 
 
-def test_env_var_set_use_env_var_no_default(app):
+def test_env_var_set_use_env_var_no_default(app, assert_parse_args):
     @app.default
     def foo(bar: Annotated[int, Parameter(env_var="BAR")]):
         pass
 
     os.environ["BAR"] = "456"
-
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind(456)
-
-    actual_command, actual_bind = app.parse_args([])
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "", 456)
 
     os.environ.pop("BAR", None)
-
     with pytest.raises(MissingArgumentError):
         app.parse_args([], exit_on_error=False)
 
 
-def test_env_var_list_set_use_env_var(app):
+def test_env_var_list_set_use_env_var(app, assert_parse_args):
     @app.default
     def foo(bar: Annotated[int, Parameter(env_var=["BAR", "BAZ"])] = 123):
         pass
@@ -70,15 +51,10 @@ def test_env_var_list_set_use_env_var(app):
     os.environ.pop("BAR", None)
     os.environ["BAZ"] = "456"
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind(456)
-
-    actual_command, actual_bind = app.parse_args([])
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, [], 456)
 
 
-def test_env_var_unset_list_use_signature_default(app):
+def test_env_var_unset_list_use_signature_default(app, assert_parse_args):
     @app.default
     def foo(bar: Annotated[int, Parameter(env_var=["BAR", "BAZ"])] = 123):
         pass
@@ -86,9 +62,4 @@ def test_env_var_unset_list_use_signature_default(app):
     os.environ.pop("BAR", None)
     os.environ.pop("BAZ", None)
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind()
-
-    actual_command, actual_bind = app.parse_args([])
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, [])
--- tests/test_bind_list.py
+++ tests/test_bind_list.py
@@ -1,75 +1,42 @@
-import inspect
 from typing import List, Optional
 
 
-def test_pos_list(app):
+def test_pos_list(app, assert_parse_args):
     @app.command
     def foo(a: List[int]):
         pass
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind([1, 2, 3])
-
-    actual_command, actual_bind = app.parse_args("foo 1 2 3")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "foo 1 2 3", [1, 2, 3])
 
 
-def test_keyword_list(app):
+def test_keyword_list(app, assert_parse_args):
     @app.command
     def foo(a: List[int]):
         pass
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind([1, 2, 3])
-
-    actual_command, actual_bind = app.parse_args("foo --a=1 --a=2 --a 3")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "foo --a=1 --a=2 --a 3", [1, 2, 3])
 
 
-def test_keyword_list_mutable_default(app):
+def test_keyword_list_mutable_default(app, assert_parse_args):
     @app.command
     def foo(a: List[int] = []):  # noqa: B006
         pass
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind([1, 2, 3])
-
-    actual_command, actual_bind = app.parse_args("foo --a=1 --a=2 --a 3")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
-
-    expected_bind = signature.bind()
-
-    actual_command, actual_bind = app.parse_args("foo")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "foo --a=1 --a=2 --a 3", [1, 2, 3])
+    assert_parse_args(foo, "foo")
 
 
-def test_keyword_list_pos(app):
+def test_keyword_list_pos(app, assert_parse_args):
     @app.command
     def foo(a: List[int]):
         pass
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind([1, 2, 3])
-
-    actual_command, actual_bind = app.parse_args("foo 1 2 3")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
+    assert_parse_args(foo, "foo 1 2 3", [1, 2, 3])
 
 
-def test_keyword_optional_list_none_default(app):
+def test_keyword_optional_list_none_default(app, assert_parse_args):
     @app.command
     def foo(a: Optional[List[int]] = None):
-        assert a is None
+        pass
 
-    signature = inspect.signature(foo)
-    expected_bind = signature.bind()
-
-    actual_command, actual_bind = app.parse_args("foo")
-    assert actual_command == foo
-    assert actual_bind == expected_bind
-
-    foo(*actual_bind.args, **actual_bind.kwargs)
+    assert_parse_args(foo, "foo")
