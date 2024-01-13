d94ef118b3fb633311579f3069e2c5070ad39ab9
Brian Pugh <bnp117@gmail.com>
2024-01-13 13:23:41 -0800

    more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,20 @@
 import inspect
 from typing import List
 
+import pytest
+
 from cyclopts.utils import ParameterDict
 
 
-def test_parameter_dict_immutable():
+@pytest.fixture
+def parameter_dict():
+    return ParameterDict()
+
+
+def test_parameter_dict_immutable(parameter_dict):
     def foo(a: int, b: int = 3):
         pass
 
-    parameter_dict = ParameterDict()
     parameters = dict(inspect.signature(foo).parameters)
 
     for name, parameter in parameters.items():
@@ -22,15 +28,16 @@ def test_parameter_dict_immutable():
     assert parameters["b"] in parameter_dict
 
 
-def test_parameter_dict_mutable():
+def test_parameter_dict_mutable(parameter_dict):
     def foo(a: int, b: List[int] = []):  # noqa: B006
         pass
 
-    parameter_dict = ParameterDict()
     parameters = dict(inspect.signature(foo).parameters)
 
     for name, parameter in parameters.items():
         parameter_dict[parameter] = name
+
+    assert len(parameter_dict) == 2
 
     for name, parameter in parameters.items():
         assert parameter_dict[parameter] == name
@@ -38,3 +45,18 @@ def test_parameter_dict_mutable():
     # Test __contains__
     assert parameters["a"] in parameter_dict
     assert parameters["b"] in parameter_dict
+
+    del parameter_dict[parameters["a"]]
+    assert parameters["a"] not in parameter_dict
+
+
+def test_parameter_dict_invalid_key(parameter_dict):
+    with pytest.raises(TypeError):
+        parameter_dict["foo"]
+
+
+def test_parameter_dict_invalid_contains(parameter_dict):
+    with pytest.raises(TypeError):
+        # Assert is only here to make the linter happy.
+        # Tests __contains__ magic method.
+        assert "foo" not in parameter_dict  # pyright: ignore[reportUnusedExpression]
