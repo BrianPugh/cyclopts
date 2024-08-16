729f2b92762ce5299f3955f2ea7a3f911877be87
Florian Daude <floriandaude@hotmail.fr>
2024-08-16 12:51:42 +0200

    passed down the version to the help/version subcommands

--- cyclopts/core.py
+++ cyclopts/core.py
@@ -330,6 +330,7 @@ class App:
                 name=self._version_flags,
                 help_flags=[],
                 version_flags=[],
+                version=self.version,
                 help="Display application version.",
             )
 
@@ -347,6 +348,7 @@ class App:
                 name=self._help_flags,
                 help_flags=[],
                 version_flags=[],
+                version=self.version,
                 help="Display this message and exit.",
             )
 
