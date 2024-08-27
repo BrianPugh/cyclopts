2f8ba138b5f7e8c3eb3e90cc5826852429ec65bd
Brian Pugh <bnp117@gmail.com>
2024-08-27 17:05:45 -0400

    lazily call _default_version; it can be quite slow to call so often.

--- tests/test_app_name_derivation.py
+++ tests/test_app_name_derivation.py
@@ -12,13 +12,12 @@ def test_app_name_derivation_main_module(mocker, mock_get_root_module_name):
     mocker.patch("cyclopts.core.sys.argv", ["__main__.py"])
     app = App()
 
-    mock_get_root_module_name.assert_called()
     assert app.name == ("mock_module_name",)
+    mock_get_root_module_name.assert_called()
 
 
-def test_app_name_derivation_not_main_module(mocker, mock_get_root_module_name):
+def test_app_name_derivation_not_main_module(mocker):
     mocker.patch("cyclopts.core.sys.argv", ["my-script.py"])
     app = App()
 
-    mock_get_root_module_name.assert_called()
     assert app.name == ("my-script.py",)
