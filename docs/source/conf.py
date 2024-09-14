4abfb2aa9a3a2bcadc0af3a62df4a046bc046650
Brian Pugh <bnp117@gmail.com>
2024-09-14 10:16:42 -0400

    Display version in docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -170,7 +170,7 @@ html_theme_options = {
     # "analytics_id": "G-XXXXXXXXXX",  # Provided by Google in your dashboard
     # "analytics_anonymize_ip": False,
     "logo_only": True,
-    "display_version": False,
+    "display_version": True,
     "prev_next_buttons_location": "bottom",
     "style_external_links": False,
     "vcs_pageview_mode": "",
