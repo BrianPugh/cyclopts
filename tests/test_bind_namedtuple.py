7a3fc1bdbfff3ef80cdee726710a9da51c068184
Brian Pugh <bnp117@gmail.com>
2024-10-02 11:08:49 -0400

    named tuple with positional arguments test.

--- tests/test_bind_namedtuple.py
+++ tests/test_bind_namedtuple.py
@@ -20,6 +20,29 @@ def test_bind_typing_named_tuple(app, assert_parse_args):
         Employee(name="John Smith", id=100),
     )
 
+    assert_parse_args(
+        foo,
+        'foo "John Smith" 100',
+        Employee(name="John Smith", id=100),
+    )
+
+
+def test_bind_typing_named_tuple_var_positional(app, assert_parse_args):
+    class Employee(NamedTuple):
+        name: str
+        id: int
+
+    @app.command
+    def foo(*users: Employee):
+        pass
+
+    assert_parse_args(
+        foo,
+        'foo "John Smith" 100 "Mary Jones" 200',
+        Employee(name="John Smith", id=100),
+        Employee(name="Mary Jones", id=200),
+    )
+
 
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="<3.10 does not have __annotations__ field.")
 def test_bind_collections_named_tuple(app, assert_parse_args):
