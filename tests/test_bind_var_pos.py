4157f112cb1f3bf6cab69f452831ae49653777b3
Brian Pugh <bnp117@gmail.com>
2025-04-23 20:20:26 -0400

    Add minimal test case to reproduce #412

--- tests/test_bind_var_pos.py
+++ tests/test_bind_var_pos.py
@@ -6,3 +6,17 @@ def test_bind_var_pos(app):
         assert tokens == ("Alice",)
 
     app(["Alice"])
+
+
+def test_bind_custom_class_only_var_positional(app, assert_parse_args):
+    """It's quite common for classes with *args to really only intend to consume 1 element."""
+
+    class MyCustomClass:
+        def __init__(self, *args: int):
+            self.args = args
+
+    @app.default
+    def default(value: MyCustomClass):
+        pass
+
+    assert_parse_args(default, "100", MyCustomClass(100))
