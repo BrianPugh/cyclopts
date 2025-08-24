868d6b4fc8a5f778abe85bf9ad6cd5f54aba310c
Brian Pugh <bnp117@gmail.com>
2025-08-23 20:32:21 -0400

    Add basic default_parameter meta-app-inheritance test.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -156,3 +156,54 @@ def test_meta_app_nested_subapp_foo_help(nested_meta_app, console, queue):
 def test_meta_app_nested_exec(nested_meta_app, queue, cmd_str, expected):
     nested_meta_app.meta(cmd_str)
     assert queue == expected
+
+
+def test_meta_app_inheriting_root_default_parameter(app, console):
+    """Confirms that default_parameter gets inherited root_app->meta_app (as well as to subapps when a meta app is used)."""
+    app.default_parameter = Parameter(negative=[])
+
+    @app.meta.default
+    def meta(
+        *tokens: Annotated[str, Parameter(show=False, allow_leading_hyphen=True)],
+        flag1: bool = False,
+    ):
+        app(tokens)
+
+    @app.command
+    def foo(*, flag2: bool = False):
+        pass
+
+    with console.capture() as capture:
+        app.meta(["--help"], console=console)
+
+    actual = capture.get()
+    expected = dedent(
+        """\
+        Usage: test_meta COMMAND
+
+        ╭─ Commands ─────────────────────────────────────────────────────────╮
+        │ foo                                                                │
+        │ --help -h  Display this message and exit.                          │
+        │ --version  Display application version.                            │
+        ╰────────────────────────────────────────────────────────────────────╯
+        ╭─ Parameters ───────────────────────────────────────────────────────╮
+        │ --flag1  [default: False]                                          │
+        ╰────────────────────────────────────────────────────────────────────╯
+        """
+    )
+    assert actual == expected
+
+    with console.capture() as capture:
+        app.meta(["foo", "--help"], console=console)
+
+    actual = capture.get()
+    expected = dedent(
+        """\
+        Usage: test_meta foo [OPTIONS]
+
+        ╭─ Parameters ───────────────────────────────────────────────────────╮
+        │ --flag2  [default: False]                                          │
+        ╰────────────────────────────────────────────────────────────────────╯
+        """
+    )
+    assert actual == expected
