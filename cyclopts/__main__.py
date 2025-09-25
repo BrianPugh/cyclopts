2597e87baca9fab633cc85b87d7cf3b6045aaede
Brian Pugh <bnp117@gmail.com>
2025-09-25 13:42:34 -0400

    Placeholder CLI skeleton.

---
+++ cyclopts/__main__.py
@@ -0,0 +1,6 @@
+"""Cyclopts CLI entry point."""
+
+from cyclopts.cli import app
+
+if __name__ == "__main__":
+    app()
---
+++ cyclopts/cli/__init__.py
@@ -0,0 +1,5 @@
+"""Cyclopts CLI implementation."""
+
+from cyclopts.cli.main import app
+
+__all__ = ["app"]
